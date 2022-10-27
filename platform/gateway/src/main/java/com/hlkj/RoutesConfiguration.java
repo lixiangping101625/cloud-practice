859aeb77fdaffe9dea2fe3a6a0521472cc2b9d2a
lixiangping <admin>
2022-10-27 17:35:08 +0800

    gateway实现限流

--- platform/gateway/src/main/java/com/hlkj/RoutesConfiguration.java
+++ platform/gateway/src/main/java/com/hlkj/RoutesConfiguration.java
@@ -1,10 +1,16 @@
 package com.hlkj;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+
+import javax.annotation.Resource;
 
 /**
  * @author Lixiangping
@@ -13,6 +19,12 @@ import org.springframework.core.annotation.Order;
  */
 @Configuration
 public class RoutesConfiguration {
+
+    @Resource
+    private KeyResolver hostNameResolver;
+    @Resource
+    @Qualifier("redisLimitItem")
+    private RateLimiter redisLimitItem;
 
     @Bean//千万不要忘了
     @Order
@@ -23,6 +35,13 @@ public class RoutesConfiguration {
         return builder.routes()
                 /** 商品controller */
                 .route(r -> r.path("/item/**"/*,"/passport/**", "/userInfo/**" */)
+                .filters(f -> f.requestRateLimiter(
+                        c -> {
+                            c.setKeyResolver(hostNameResolver);
+                            c.setRateLimiter(redisLimitItem);
+                            c.setStatusCode(HttpStatus.BAD_GATEWAY);
+                        }
+                ))
 //                .filters(f -> f.stripPrefix(1))
                 .uri("lb://CLOUD-ITEM-SERVICE"))
                 /** 用户controller */
